c62aa8532d86ac8378833abb6d762013f4c45fbd
Nick Rodriguez <nickrod518@gmail.com>
2012-12-03 17:33:55 -0500

    Used map as data structure to count names on list

--- List.cpp
+++ List.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <map>
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::ifstream;
+using std::map;
+using std::pair;
 
 int main (int argc, char** argv) {
   if (argc != 3) {
@@ -13,17 +16,26 @@ int main (int argc, char** argv) {
   } else {
     cout << "filename: " << argv[1] << endl;
     cout << "threshold: " << argv[2] << endl;
-    cout << atoi(argv[1]) + atoi(argv[2]) << endl;
-
+   
+    map<string, int> list;
+    map<string, int>::iterator it;
     string line;
 
     ifstream file(argv[1]);
     if (file.is_open()) {
       while(file.good()) {
         getline(file, line);
-        cout << line << endl;
+        
+        if (list.count(line))
+          list[line]++;
+        else
+          list.insert(pair<string, int>(line, 1));
       }
     }
+
+    cout << "NAME" << "\t: " << "OCCURRENCES" << endl;
+    for (it = list.begin(); it != list.end(); ++it)
+      cout << (*it).first << " \t: " << (*it).second << endl;
   }
 
   return 0;
