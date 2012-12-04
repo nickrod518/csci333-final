edb85cec45611ed5f24afd0feb33b464a48337e1
Nick Rodriguez <nickrod518@gmail.com>
2012-12-03 23:05:05 -0500

    Started tweet analyzer

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ CFLAGS = -Wall -Werror -Wextra
 list: List.cpp
 	$(CPP) $(CFLAGS) -o list List.cpp
 
+words: Words.cpp
+	$(CPP) $(CFLAGS) -o words Words.cpp
+
 clean:
 	rm -f *~;
 	rm -f list;
+	rm -f words;
---
+++ Words.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <map>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::map;
+using std::pair;
+
+int main (int argc, char** argv) {
+  if (argc != 3) {
+    cout << "Usage: list [in file] [out file]" << endl;
+  } else {
+    cout << "in file: " << argv[1] << endl;
+    cout << "out file: " << argv[2] << endl;
+   
+    map<string, int> words;
+    map<int, string> sortedWords;
+    map<string, int>::iterator it;
+    map<int, string>::iterator it2;
+    string line;
+    ifstream ifile(argv[1]);
+    ofstream ofile(argv[2]);
+
+    // store words in dictionary
+    if (ifile.is_open()) {
+      while(ifile.good()) {
+        getline(ifile, line);
+        
+        if (words.count(line))
+          words[line]++;
+        else
+          words.insert(pair<string, int>(line, 1));
+      }
+    }
+
+    // move words to another dictionary sorted by occurrences
+    for (it = words.begin(); it != words.end(); ++it) {
+      cout << (*it).first << endl;
+      sortedWords.insert(pair<int, string>((*it).second, (*it).first));
+    }
+
+    // move words to file in order of occurrences
+    if (ofile.is_open()) {
+      while(ofile.good()) {
+        for (it2 = sortedWords.begin(); it2 != sortedWords.end(); ++it2)
+          ofile << (*it2).second << endl;
+        ofile.close();
+      }
+    }
+  }
+
+  return 0;
+}
